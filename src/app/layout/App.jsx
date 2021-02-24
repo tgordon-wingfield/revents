667f74c184aa48748d8d666548e3f117463fa496
Thea G-W <t.gordon-wingfiel@hotmail.co.uk>
2021-02-24 16:17:07 +0000

    End of section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container } from 'semantic-ui-react';
+import EventDashboard from '../../features/events/eventDashboard/EventDashboards';
+import NavBar from '../../features/nav/NavBar';
 
 function App() {
+
+  const [formOpen, setFormOpen] = useState(false);
+
   return (
-    <div className="App">
+    <>
       <h1>Re-vents</h1>
-    </div>
+      <NavBar setFormOpen = { setFormOpen }/>
+      <Container className = 'main'>
+        <EventDashboard formOpen = { formOpen } setFormOpen = { setFormOpen }/>
+      </Container>
+      
+    </>
   );
 }
 
