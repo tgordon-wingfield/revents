8127ff13f84e1ba4fbaf614781f28278965119d2
Thea G-W <t.gordon-wingfiel@hotmail.co.uk>
2021-03-08 15:54:13 +0000

    End of Section 9

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,4 +1,5 @@
 import { Route, useLocation } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import { Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/events/eventDashboard/EventDashboards';
 import EventForm from '../../features/events/eventForm/EventForm';
@@ -14,6 +15,7 @@ function App() {
   return (
     <>
       <ModalManager/>
+      <ToastContainer position = 'bottom-right' hideProgressBar/>
       <Route exact path = '/' component = { HomePage }/>
       <Route path = { '/(.+)' } render = { () => (
         <>
---
+++ src/features/events/eventDashboard/EventFilters.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Calendar from 'react-calendar';
+import { Header, Menu } from 'semantic-ui-react';
+export default function EventFilters() {
+    return (
+        <>
+            <Menu vertical size = 'large' style = { { width: '100%' } }>
+                <Header icon = 'filter' attached color = 'teal' content = 'Filters'/>
+                <Menu.Item content = 'All Events'/>
+                <Menu.Item content = "I'm Going"/>
+                <Menu.Item content = "I'm Hosting"/>
+            </Menu>
+            <Header icon = 'calendar' attached color = 'teal' content = 'Select Date'/>
+            <Calendar/>
+        </>
+    )
+}
